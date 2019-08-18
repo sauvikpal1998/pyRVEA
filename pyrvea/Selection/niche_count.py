bbcd64957e577e547f79653f77c47152203ef375
sauvikpal1998 <sauvikpal1998@gmail.com>
2019-08-19 00:41:34 +0530

    Constraint Handeling Functionality for constraint handeling and niche count. Not tested yet.

---
+++ pyrvea/Selection/niche_count.py
@@ -0,0 +1,66 @@
+import numpy as np
+from math import sqrt
+
+def get_niche_count(
+    f_diff: np.ndarray,             # list of objectives max-min
+    min_dist: int,                  # minimum distance param
+    sigma_share: int,               # sigma share value
+    f_pop_vals: np.ndarray,         # calculated values of objective funcs. for population - shape=[objectives][pops]
+    f_selected_val: np.ndarray,     # calculated values of objective funcs. for selected individual - shape=[objectives]
+    # objectives: list,             # list of objective funcs.
+):
+
+    niche_count = 0
+    
+    for i in range(len(f_pop_vals[0])):
+        dist = 0
+        for j in range(len(f_pop_vals)):
+            dist += ((f_selected_val[j]-f_pop_vals[j][i])/(f_diff[j]))**2
+        dist = sqrt(dist)
+
+        if(dist<min_dist):
+            niche_count += 1-(dist/sigma_share)
+
+    return niche_count
+
+def best_niche_index(
+    population: list,
+    # individuals: list,
+    indi_obj_vals: np.ndarray,
+    objectives: list,
+):
+    fmax = []
+    fmin = []
+    f_pop_vals = []
+    f_temp = []
+
+    for obj in objectives:
+        temp_max = -2**9
+        temp_min = 2**9
+        f_temp = []
+        for indi in population:
+            obj_val = obj(indi)
+            f_temp.append(obj_val)
+            temp_max = max(obj_val, temp_max)
+            temp_min = min(obj_val, temp_min)
+        f_pop_vals.append(f_temp)
+        fmax.append(temp_max)
+        fmin.append(temp_min)
+    fmax = np.array(fmax)
+    fmin = np.array(fmin)
+    f_pop_vals = np.array(f_pop_vals)
+
+    f_diff = fmax-fmin
+
+    # TODO: How to optimize niche count calculation
+
+    # params set
+    sigma_share = 0.2
+    min_dist = 0.5
+
+    niche_counts = []
+
+    for f_selected_val in indi_obj_vals:
+        niche_counts.append(get_niche_count(f_diff, min_dist, sigma_share, f_pop_vals, f_selected_val))
+
+    return niche_counts.index(min(niche_counts))
--- pyrvea/Selection/tournament_select.py
+++ pyrvea/Selection/tournament_select.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from math import sqrt
+from pyrvea.Selection import niche_count
 
 def tour_select(fitness, tournament_size):
     """Tournament selection. Choose number of individuals to participate
@@ -24,3 +25,126 @@ def tour_select(fitness, tournament_size):
     chosen.sort(key=lambda x: x[1])
 
     return chosen[0][0]
+
+def constraint_tour_select(
+    constraints: list,
+    # individuals: list,
+    population: list,
+    objectives: list,
+):
+    """
+    A binary Tournament constraint selection method.
+
+    Parameters
+    ----------
+    contraints : array_like
+        An array of contraint functions.
+    population : array_like
+        The total population
+    objectives : array_like
+        Objective functions
+    
+    Returns
+    -------
+    array_like
+        new population
+    """
+
+    new_population = []
+
+    while(len(population)>1):
+        individuals = []
+        for i in range(2):
+            index = np.random.randint(0, len(population)-1)
+            individuals.append(population[index])
+            population.pop(index)
+
+
+        feasibility = [True, True]
+
+        # check feasibility
+
+        for constraint in constraints:
+            if constraint(individuals[0]) == False:
+                feasibility[0] = False
+            if constraint(individuals[1]) == False:
+                feasibility[1] = False
+
+        # if feasibilty is not same
+
+        if feasibility[0] != feasibility[1]:
+            if feasibility[0]:
+                new_population.append(individuals[0])
+            else:
+                new_population.append(individuals[1])
+
+        # if the feasibilty of both of them is same
+
+        elif feasibility[0] == feasibility[1]:
+            comparison_set = []
+            comparison_set_type = feasibility[0]
+
+            # create a comparison set
+
+            while len(comparison_set)<10:
+                individual = np.random.choice(population)
+                indi_feasibility = True
+
+                for constraint in constraints:
+                    if constraint(individual) != comparison_set_type:
+                        indi_feasibility = False
+                
+                if indi_feasibility:
+                    comparison_set.append(individual)
+
+            dominance = [True, True]
+
+            comparison_set_objective_vals = []
+
+            # find dominance
+
+            for comparitive in comparison_set:
+                objective_vals = []
+                for obj in objectives:
+                    objective_vals.append(obj(comparitive))
+                comparison_set_objective_vals.append(objective_vals)
+
+            selected_individuals_obj_vals = []
+
+            for i, individual in enumerate(individuals):
+                objective_vals = []
+                for obj in objectives:
+                    objective_vals.append(obj(comparitive))
+                flag = False
+
+                selected_individuals_obj_vals.append(objective_vals)
+
+                for cs_indi_vals in comparison_set_objective_vals:
+                    for obj_val, cs_val in zip(objective_vals, cs_indi_vals):
+                        if(obj_val>=cs_val):
+                            flag = True
+                            break
+                    if flag:
+                        break
+                
+                if flag==False:
+                    dominance[i] = flag
+            
+            # check if dominance is same
+
+            if(dominance[0]!=dominance[1]):
+                if dominance[0]:
+                    new_population.append(individuals[0])
+                else:
+                    new_population.append(individuals[1])
+                
+            # niche count comparison if both the values are same
+
+            else:
+                new_population.append(individuals[niche_count.best_niche_index(population, selected_individuals_obj_vals, objectives)])
+
+    return new_population      
+
+
+                
+        
