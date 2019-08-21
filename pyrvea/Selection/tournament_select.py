9d5a4b3819530da31e04644ed459e1f9535fa43f
sauvikpal1998 <sauvikpal1998@gmail.com>
2019-08-21 15:45:55 +0530

    Bugfixes in Constraint handeling

--- pyrvea/Selection/tournament_select.py
+++ pyrvea/Selection/tournament_select.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import sqrt
 from pyrvea.Selection import niche_count
+import copy
 
 def tour_select(fitness, tournament_size):
     """Tournament selection. Choose number of individuals to participate
@@ -18,7 +19,7 @@ def tour_select(fitness, tournament_size):
     int
         The index of the best individual.
     """
-    aspirants = np.random.choice(len(fitness)-1, tournament_size, replace=False)
+    aspirants = np.random.choice(len(fitness) - 1, tournament_size, replace=False)
     chosen = []
     for ind in aspirants:
         chosen.append([ind, fitness[ind]])
@@ -26,12 +27,8 @@ def tour_select(fitness, tournament_size):
 
     return chosen[0][0]
 
-def constraint_tour_select(
-    constraints: list,
-    # individuals: list,
-    population: list,
-    objectives: list,
-):
+
+def constraint_tour_select(constraints: list, population: list, objectives: list):
     """
     A binary Tournament constraint selection method.
 
@@ -49,16 +46,18 @@ def constraint_tour_select(
     array_like
         new population
     """
-
+    alt_population = list(population)
     new_population = []
-
-    while(len(population)>1):
+    # count = 0
+    while len(alt_population) > 2:
+        print(len(alt_population))
+        # count = count + 1
         individuals = []
         for i in range(2):
-            index = np.random.randint(0, len(population)-1)
-            individuals.append(population[index])
-            population.pop(index)
-
+            index = np.random.randint(0, len(alt_population) - 1)
+            individuals.append(alt_population[index])
+            alt_population.pop(index)
+            # alt_population = alt_population.delete(alt_population, index, 0)
 
         feasibility = [True, True]
 
@@ -73,6 +72,7 @@ def constraint_tour_select(
         # if feasibilty is not same
 
         if feasibility[0] != feasibility[1]:
+            print("feasibility")
             if feasibility[0]:
                 new_population.append(individuals[0])
             else:
@@ -86,14 +86,15 @@ def constraint_tour_select(
 
             # create a comparison set
 
-            while len(comparison_set)<10:
-                individual = np.random.choice(population)
+            while len(comparison_set) < 10:
+                rand_num = np.random.randint(len(population))
+                individual = population[rand_num]
                 indi_feasibility = True
 
                 for constraint in constraints:
                     if constraint(individual) != comparison_set_type:
                         indi_feasibility = False
-                
+
                 if indi_feasibility:
                     comparison_set.append(individual)
 
@@ -108,7 +109,7 @@ def constraint_tour_select(
                 for obj in objectives:
                     objective_vals.append(obj(comparitive))
                 comparison_set_objective_vals.append(objective_vals)
-
+            # print(comparison_set_objective_vals)
             selected_individuals_obj_vals = []
 
             for i, individual in enumerate(individuals):
@@ -120,31 +121,39 @@ def constraint_tour_select(
                 selected_individuals_obj_vals.append(objective_vals)
 
                 for cs_indi_vals in comparison_set_objective_vals:
+                    print('------')
                     for obj_val, cs_val in zip(objective_vals, cs_indi_vals):
-                        if(obj_val>=cs_val):
+                        print('obj-vals=', obj_val,' cs-val=', cs_val)
+                        if obj_val >= cs_val:
                             flag = True
                             break
                     if flag:
                         break
-                
-                if flag==False:
+                print(flag)
+                if flag == False:
                     dominance[i] = flag
-            
+
             # check if dominance is same
 
-            if(dominance[0]!=dominance[1]):
+            if dominance[0] != dominance[1]:
+                # print("dominance")
                 if dominance[0]:
                     new_population.append(individuals[0])
                 else:
                     new_population.append(individuals[1])
-                
+
             # niche count comparison if both the values are same
 
             else:
-                new_population.append(individuals[niche_count.best_niche_index(population, selected_individuals_obj_vals, objectives)])
+                # print(dominance)
+                # print("niche count")
+                new_population.append(
+                    individuals[
+                        niche_count.best_niche_index(
+                            population, selected_individuals_obj_vals, objectives
+                        )
+                    ]
+                )
 
-    return new_population      
+    return new_population
 
-
-                
-        
